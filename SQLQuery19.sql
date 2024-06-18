800c5e92221f7d67662f9de78e8f3c0cf2a52884
sj49ner <sj49ner@gmail.com>
2024-06-18 00:11:56 -0700

    Create SQLQuery19.sql

---
+++ SQLQuery19.sql
@@ -0,0 +1,19 @@
+SELECT [NameStyle]
+      ,[Title]
+      ,[FirstName]
+      ,[MiddleName]
+      ,[LastName]
+      ,[Suffix]
+      ,[CompanyName]
+      ,[SalesPerson]
+      ,[EmailAddress]
+      ,[Phone]
+      ,[PasswordHash]
+      ,[PasswordSalt]
+      ,[rowguid]
+      ,[ModifiedDate]
+  FROM [SalesLT].[Customer]
+
+  EXECUTE AS User = 'TestUser'
+
+  Revert
